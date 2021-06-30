16822c368cddcf712f2323d22c09fe50d9ae83c3
Ivana Dordevic <ivanadordevic.usa@gmail.com>
2021-06-30 19:32:14 -0400

    adding more review

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,7 +3,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
-        System.out.println("Recapping like day 1");
+        System.out.println("Recapping like day 1, I wanted to add more");
+
 
     }
 }
